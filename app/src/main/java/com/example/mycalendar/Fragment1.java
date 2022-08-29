0c7896cc0413c5826fbc4b5323b943752cf284f3
user <hyeonsueo@gmail.com>
2022-08-29 10:17:19 +0900

    파일 처리 추가

--- app/src/main/java/com/example/mycalendar/Fragment1.java
+++ app/src/main/java/com/example/mycalendar/Fragment1.java
@@ -1,5 +1,6 @@
 package com.example.mycalendar;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,54 +9,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment1#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Fragment1 extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragment1() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment1 newInstance(String param1, String param2) {
-        Fragment1 fragment = new Fragment1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    //변수 선언
+    String fileName;
+    Button btn_write;
+    EditText editDiary;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -64,19 +32,65 @@ public class Fragment1 extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_1, container, false);
         //
         DatePicker dp = view.findViewById(R.id.datePicker);
+        editDiary = view.findViewById(R.id.editDiary);
+        btn_write = view.findViewById(R.id.btn_write);
+
         int Year = dp.getYear();
         int Month = dp.getMonth();
         int Day = dp.getDayOfMonth();
 
         Log.d("DATE", Year + Month + Day + "");
 
+        fileName = Year + "_" + (Month + 1) + "_" + Day + ".txt";
         dp.init(Year, Month, Day, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                Log.d("CLICK:", year + "_" + monthOfYear + "_" + dayOfMonth);
+                Log.d("CLICK:", year + "_" + (monthOfYear + 1) + "_" + dayOfMonth);
+                fileName = year + "_" + (monthOfYear + 1) + "+" + dayOfMonth + ".txt";
+
+                String str = readDiary(fileName);
+                editDiary.setText(str); //화면에 뿌려준다.
+
             }
         });
+
+        btn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //fileName에 해당하는 파일을 쓰기모드로 editDiary에 입력된 텍스트를 읽어서 쓴다.
+                try {
+                    FileOutputStream outfs = getContext().openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str = editDiary.getText().toString();
+                    outfs.write(str.getBytes());
+                    outfs.close();
+                    Toast.makeText(getContext(), fileName + "파일을 저장했습니다.", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
         return view;
+    }
+
+    //선택한 날짜에 해당하는 읽기 파일의 이름을 넘겨 받아서
+    //그 파일이 있으면 보여주고 없으면 보여주지 않음
+    private String readDiary(String fileName) {
+        String diaryStr = null;
+        //파일 처리
+        try {
+            FileInputStream infS = getContext().openFileInput(fileName); //열고
+            byte[] txt = new byte[500];
+            infS.read(txt); //읽고
+            infS.close(); //닫고
+
+            diaryStr =  new String(txt);
+        } catch (IOException e) {
+            editDiary.setText("일기 없음");
+        }
+
+        return diaryStr;
     }
 
 //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
